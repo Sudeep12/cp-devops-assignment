a00c8d6122c275bb27e50f214c637c122dec4082
Sudeep.Shetty <Sudeep.Shetty@lseg.com>
2025-10-29 13:30:40 +0530

    block public access removed

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -27,11 +27,11 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "s3" {
 }
 
 # Block public access
-resource "aws_s3_bucket_public_access_block" "s3" {
-  bucket = aws_s3_bucket.s3.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
+#resource "aws_s3_bucket_public_access_block" "s3" {
+#  bucket = aws_s3_bucket.s3.id
+#
+#  block_public_acls       = true
+#  block_public_policy     = true
+#  ignore_public_acls      = true
+#  restrict_public_buckets = true
+#}
